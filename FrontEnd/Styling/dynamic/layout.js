731fa2db23bfe8cfcdd20d54ab95a0f6eb700dc3
MarianaAAW <aamariana1907@gmail.com>
2025-01-24 22:24:05 +0100

    done

--- FrontEnd/Styling/dynamic/layout.js
+++ FrontEnd/Styling/dynamic/layout.js
@@ -3,31 +3,17 @@ function active_link(n) {
 
         let A1 = document.getElementById("A1")
         let A2 = document.getElementById("A2")
-        let A3 = document.getElementById("A3")
-
 
         A1.className += " active"
         A2.className = "nav-link"
-        A3.className = "nav-link"
 
     }
     if (n === 2) {
         let A1 = document.getElementById("A1")
         let A2 = document.getElementById("A2")
-        let A3 = document.getElementById("A3")
 
         A1.className = "nav-link"
         A2.className += " active"
-        A3.className = "nav-link"
-
-    } if (n === 3) {
-        let A1 = document.getElementById("A1")
-        let A2 = document.getElementById("A2")
-        let A3 = document.getElementById("A3")
-
-        A1.className = "nav-link"
-        A2.className = "nav-link"
-        A3.className += " active"
 
     }
 }
